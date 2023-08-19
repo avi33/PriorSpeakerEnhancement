83714407e4fa2bfe194195ad30c0a0274f7513fa
avi <avi.gazneli@gmail.com>
2023-08-19 19:47:18 +0300

    backup

---
+++ datasets/data_utils.py
@@ -0,0 +1,25 @@
+import torch
+
+def create_dataset(args):
+    if args.dataset == 'cmuarctic':        
+        from cmudata import CMUDataset as Dataset        
+        train_set = Dataset(root=r"/media/avi/8E56B6E056B6C86B/datasets/ARCTIC8k", 
+                            mode='train', 
+                            segment_length=args.seq_len, 
+                            sampling_rate=args.sampling_rate, 
+                            transforms=None)
+        
+        test_set = Dataset(root=r"/media/avi/8E56B6E056B6C86B/datasets/ARCTIC8k", 
+                           mode='test', 
+                           segment_length=args.seq_len, 
+                           sampling_rate=args.sampling_rate, 
+                           augment=None)
+        
+    elif args.dataset == 'vctk':
+        train_set = None
+        test_set = None
+                
+    else:
+        raise ValueError("wrong dataset {}".format(args.dataset))
+    
+    return train_set, test_set
